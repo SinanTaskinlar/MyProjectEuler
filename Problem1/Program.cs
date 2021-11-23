05b2f3effadb56ea7f4015a89f8dfc5e20b5c573
Sinan Taşkınlar <sinantasknlar@gmail.com>
2021-11-24 00:39:09 +0300

    Pr1&Pr2

---
+++ Problem1/Program.cs
@@ -0,0 +1,4 @@
+﻿var total = 0;
+for (int i = 1; i < 1000; i++)
+	total += (i % 3 == 0 || i % 5 == 0) ? i : 0;
+Console.Out.WriteLine(total);
---
+++ Problem2/Program.cs
@@ -0,0 +1,11 @@
+﻿long number1 = 1, number2 = 1, result = 0, total = 0;
+
+while (result < 4000000)
+{
+	total += result % 2 == 0 ? result : 0;
+	result = number1 + number2;
+	number2 = number1;
+	number1 = result;
+}
+
+Console.Out.WriteLine(total);
