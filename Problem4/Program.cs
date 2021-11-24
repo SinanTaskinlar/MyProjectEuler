41e49f607130945fcd5aa7dafe12edd7889395f2
Sinan Taşkınlar <sinantasknlar@gmail.com>
2021-11-24 13:27:26 +0300

    Problem4

--- Problem4/Program.cs
+++ Problem4/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int min = 100,max = 999, multiple = 0;
+List<int> multiples = new List<int>();
+
+for (int i = min; i < max; i++)
+{
+	for (int j = min; j < max; j++)
+	{
+		multiple = i * j;
+		string reversed = new string(multiple.ToString().ToCharArray().Reverse().ToArray());
+		if (reversed == multiple.ToString())
+		{
+			multiples.Add(multiple);
+		}
+	}
+}
+Console.WriteLine(multiples.Max());
