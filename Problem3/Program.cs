b0288037c4fe6e075c4cc7cb70e0e4eb01a3f2a9
Sinan Taşkınlar <sinantasknlar@gmail.com>
2021-11-24 13:21:09 +0300

    Problem8

---
+++ Problem3/Program.cs
@@ -0,0 +1,23 @@
+﻿//need to be faster
+long number = 600851475143;
+long largestPrime = 0;
+for (long i = 2; i < number; i++)
+{
+	if (number % i == 0)
+	{
+		bool isPrime = true;
+		for (long j = 2; j < i; j++)
+		{
+			if (i % j == 0)
+			{
+				isPrime = false;
+				break;
+			}
+		}
+		if (isPrime)
+		{
+			largestPrime = i;
+		}
+	}
+}
+Console.Out.WriteLine(largestPrime);
